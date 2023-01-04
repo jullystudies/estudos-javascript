62cbbea78ac5c9de916bc486f8450e09fac84c33
Jullyane Amaral <jullyanea.amaral@gmail.com>
2023-01-04 14:28:07 -0300

    04/01/23 2:27 pm

---
+++ Programador FullStack - OneBitCode/basicos/aula 10/index.js
@@ -0,0 +1,10 @@
+let nome = "JUJU"
+
+//console.log(nome)
+//console.error(nome)
+//.warn(nome)
+
+//window.alert(nome)
+//nome = window.prompt('Insira o seu nome?')
+let confirmacao = window.confirm('Confirma essa acao')
+
